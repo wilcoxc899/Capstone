2aac8b85826ba4eb620b21baaeb64feeeb936f76
nejedimu <112342049+nejedimu@users.noreply.github.com>
2024-04-26 11:35:04 -0500

    Add files via upload

--- lidar_code.py
+++ lidar_code.py
@@ -34,31 +34,33 @@ def Motor_Speed(pca,percent):
 
 def all_sensors(data):
     for angle, distance in enumerate(data):
-        b = scan_data[270] # distance at zero angle relative to the car's x-axis
-        a = scan_data[270 + theta] # distance at theta
-        alpha = atan((a*cos(theta))-b, a*sin(theta)) # offset of the car: should be zero if car is straight
-        D = b*cos(theta) # current distance between the car and the right wall
-        D_new = D + (L*sin(alpha)) # future projectd distance
+        if distance > 0:
+            b = scan_data[270] # distance at zero angle relative to the car's x-axis
+            a = scan_data[270 + theta] # distance at theta
+            print(f'a,b: {a},{b}')
+            alpha = atan((a*cos(theta))-b, a*sin(theta)) # offset of the car: should be zero if car is straight
+            D = b*cos(theta) # current distance between the car and the right wall
+            D_new = D + (L*sin(alpha)) # future projectd distance
 
         # Control algorithm
-        error = 1000 - D_new
-        integral += error
-        derivative = error - pre_error
-        pre_error = error
-        control = (kp*error) + (ki*integral) + k(kd*derivative)
+            error = 1000 - D_new
+            integral += error
+            derivative = error - pre_error
+            pre_error = error
+            control = (kp*error) + (ki*integral) + k(kd*derivative)
 
-        servo7.angle = 86.5 + control
-        car_angle = servo7.angle
+            servo7.angle = 86.5 + control
+            car_angle = servo7.angle
 
         # changing the speed of the car to slow down as the turn angle increases  
-        if (76.5 <= car_angle <= 86.5):
-            Motor_Speed(pca, 0.2)
-        elif (66.5 < car_angle < 76.5):
-            Motor_Speed(pca, 0.18)
-        elif (car_angle <= 66.5):
-            Motor_Speed(pca,0.15)
-        else:
-            Motor_Speed(pca,0)
+            if (76.5 <= car_angle <= 86.5):
+                Motor_Speed(pca, 0.2)
+            elif (66.5 < car_angle < 76.5):
+                Motor_Speed(pca, 0.18)
+            elif (car_angle <= 66.5):
+                Motor_Speed(pca,0.15)
+            else:
+                Motor_Speed(pca,0)
 
 scan_data = [0]*360
 
